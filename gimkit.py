78b9f520bd6f95f380d28aa0f58fa63279c03ca9
Alek <amacwilliams06@gmail.com>
2021-12-22 16:58:34 -0500

    Create gimkit.py

---
+++ gimkit.py
@@ -0,0 +1,43 @@
+import requests
+import random
+import time
+import threading
+import json
+
+tokenFile = open("tokens.txt", "w")
+idFile = open("ids.txt", "w")
+
+def namegen():
+    length = random.randint(7, 15) #set your min and max (min, max)
+    eval = "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
+    "S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0",""
+
+
+    return ''.join(random.choice(eval) for i in range(length))
+
+link = "https://www.gimkit.com/api/register"
+
+data = {
+    "accountType": "educator",
+    "areaOfExpertise": "Communications",
+    "bulkSubscriptionToJoin": "",
+    "country": "US",
+    "dateOfBirth": "",
+    "districtId": "",
+    "email": namegen()+"@gmail.com",
+    "firstName": namegen(),
+    "googleToken": "",
+    "gradeLevel": "High School",
+    "groupJoining": "",
+    "guardianEmail": "",
+    "lastName": namegen(),
+    "organization": "",
+    "password": namegen(),
+    "schoolId": ""
+}
+signup = requests.post(link,data)
+token = json.loads(signup.text)
+tokenFile.write(token['token']+ "\n")
+id = json.loads(signup.text)
+idFile.write(id['_id']+ "\n")
+print(signup.text)
