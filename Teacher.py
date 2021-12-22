14dbe64a153d0aee6bf81bdfd0ef21732269889d
Alek <amacwilliams06@gmail.com>
2021-12-22 17:18:49 -0500

    Update Teacher.py

--- Teacher.py
+++ Teacher.py
@@ -15,9 +15,10 @@ def namegen():
 
     return ''.join(random.choice(eval) for i in range(length))
 
-link = "https://www.gimkit.com/api/register"
+def SignUp():
+    link = "https://www.gimkit.com/api/register"
 
-data = {
+    data = {
     "accountType": "educator",
     "areaOfExpertise": "Communications",
     "bulkSubscriptionToJoin": "",
@@ -34,10 +35,25 @@ data = {
     "organization": "",
     "password": namegen(),
     "schoolId": ""
-}
-signup = requests.post(link,data)
-token = json.loads(signup.text)
-tokenFile.write(token['token']+ "\n")
-id = json.loads(signup.text)
-idFile.write(id['_id']+ "\n")
-print(signup.text)
+    }
+    signup = requests.post(link,data)
+    token = json.loads(signup.text)
+    tokenFile.write(token['token']+ "\n")
+    id = json.loads(signup.text)
+    idFile.write(id['_id']+ "\n")
+    print(signup.text)
+   
+threads=[]
+
+
+for i in range(100000):
+  t = threading.Thread(target = SignUp)
+  t.Daemon = True
+  threads.append(t)
+
+for i in range(100000):
+  threads[i].start()
+  time.sleep(0.2)
+
+for i in range(100000):
+  threads[i].join()
