15cbc258d0b53ce20aad34ca4fb175837dcb4083
Alek <amacwilliams06@gmail.com>
2021-12-22 17:00:20 -0500

    Create student.py

---
+++ student.py
@@ -0,0 +1,57 @@
+from os import name
+import requests
+import random
+import time
+import threading
+import json
+
+tokenFile = open("tokens.txt", "w")
+idFile = open("ids.txt", "w")
+
+def namegen():
+    length = random.randint(7, 15) #set your min and max (min, max)
+    eval = "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
+    "S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0",""
+
+
+    return ''.join(random.choice(eval) for i in range(length))
+
+def SignUp():
+    link = "https://www.gimkit.com/api/register"
+
+    data = {
+    "accountType": "student",
+    "areaOfExpertise": "",
+    "bulkSubscriptionToJoin": "",
+    "country": "",
+    "dateOfBirth": "",
+    "districtId" : "" ,
+    "email": namegen()+"@gmail.com",
+    "firstName": namegen(),
+    "googleToken": "",
+    "gradeLevel": "",
+    "groupJoining": "",
+    "guardianEmail": "",
+    "lastName": "r",
+    "organization": "",
+    "password": namegen(),
+    "schoolId": ""
+    }
+    signup = requests.post(link,data)
+    print(signup.text)
+
+threads=[]
+
+
+for i in range(100000):
+  t = threading.Thread(target = SignUp)
+  t.Daemon = True
+  threads.append(t)
+
+for i in range(100000):
+  threads[i].start()
+  time.sleep(0.2)
+
+for i in range(100000):
+  threads[i].join()
+
